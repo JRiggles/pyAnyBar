649566acce17005ae0b637e12dd42f3f9777b728
John Riggles <jriggles@icloud.com>
2022-09-07 14:42:41 -0400

    Update anybar.py
    refactor!: update code format, rename 'Anybar.change()' to 'Anybar.set()'

--- anybar/anybar.py
+++ anybar/anybar.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python
-
 import socket
 
-colors = ['white', 'red', 'orange', 'yellow', 'green', 'cyan', 'blue',
-          'purple', 'black', 'question', 'exclamation', 'none', 'filled',
-          'hollow']
+
+colors = [
+    'white', 'red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple',
+    'black', 'question', 'exclamation', 'none', 'filled', 'hollow'
+]
 
 
 class AnyBar():
     def __init__(self, port=1738, address='localhost'):
         self.port = port
         self.address = address
-        self.socket = socket.socket(socket.AF_INET,
-                                    socket.SOCK_DGRAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def change(self, color, text=None):
+    def set(self, color, text=None):
         if color not in colors:
-            raise ValueError('Color is not valid. It must be one of the '
-                             'following: {}'.format(', '.join(colors)))
+            raise ValueError(
+                ('Color is not valid. It must be one of the '
+                 f'following: {", ".join(colors)}')
+        )
 
         if text is None:
-            self.socket.sendto(color.encode('utf-8'),
-                               (self.address, self.port))
+            self.socket.sendto(
+                color.encode('utf-8'),
+                (self.address, self.port)
+            )
         else:
-            if type(text) is unicode:
-                message = u'{} {}'.format(color, text).encode('utf-8')
-            else:
-                message = '{} {}'.format(color, text)
+            message = f'{color} {text}'
             self.socket.sendto(message, (self.address, self.port))
 
 
-
 if __name__ == '__main__':
-    import argparse
+    from argparse import ArgumentParser
 
-    parser = argparse.ArgumentParser(description='Change color of AnyBar')
-    parser.add_argument('color', choices=colors,
-                        help='The color you want to change to.')
-    parser.add_argument('port', type=int, nargs='?', default=1738,
-                        help='The port of the AnyBar instance '
-                             '(default: 1738).')
-    parser.add_argument('address', nargs='?', default='localhost',
-                        help='The address of the AnyBar instance '
-                             '(default: localhost).')
+    parser = ArgumentParser(description='Change the color of AnyBar')
+    parser.add_argument(
+        'color',
+        choices=colors,
+        help='The color you want to change to.'
+    )
+    parser.add_argument(
+        'port',
+        type=int,
+        nargs='?',
+        default=1738,
+        help='The port of the AnyBar instance (default: 1738).'
+    )
+    parser.add_argument(
+        'address',
+        nargs='?',
+        default='localhost',
+        help='The address of the AnyBar instance (default: localhost).'
+    )
     args = parser.parse_args()
 
-    AnyBar(port=args.port, address=args.address).change(args.color)
+    AnyBar(port=args.port, address=args.address).set(args.color)
+
